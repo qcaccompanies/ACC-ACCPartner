75380a2fb7b92adce81b9c3c098c4d7eff3fe984
geraldakensadhani <geraldakensadhani@yahoo.com>
2020-01-24 12:35:44 +0700

    Update Baru [Change and Forgot Password]

---
+++ Scripts/Change Password/Script1579779429745.groovy
@@ -0,0 +1,71 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.callTestCase(findTestCase('Login'), [('var_username') : var_username, ('var_password') : var_password, ('expected_login') : ''
+        , ('status_login') : '', ('press_back_button') : '', ('close_app') : ''], FailureHandling.STOP_ON_FAILURE)
+
+if (expected_login == 'passed') {
+    Mobile.tap(findTestObject('Change Password/tap_akun'), 0)
+
+    Mobile.tap(findTestObject('Change Password/tap_dealer_id'), 0)
+
+    Mobile.tap(findTestObject('Change Password/button_change_password'), 0)
+
+    Mobile.tap(findTestObject('Change Password/tap_old_password'), 0)
+
+    Mobile.setText(findTestObject('Change Password/input_old_password'), var_old_password, 0)
+
+    Mobile.tap(findTestObject('Change Password/tap_new_password'), 0)
+
+    Mobile.setText(findTestObject('Change Password/input_new_password'), var_new_password, 0)
+
+    Mobile.tap(findTestObject('Change Password/tap_confirm_new_password'), 0)
+
+    Mobile.setText(findTestObject('Change Password/input_confirm_new_password'), var_confirm_new_password, 0)
+
+    Mobile.tap(findTestObject('Change Password/button_confirm_change_password'), 0)
+
+    if (expected_change_password == 'passed') {
+        Mobile.verifyElementVisible(findTestObject('Change Password/warn_change_password_success'), 0)
+    } else if (expected_change_password == 'failed') {
+        switch (true) {
+            case 'EmptyField':
+                Mobile.verifyElementVisible(findTestObject('Change Password/warn_empty_password'), 0)
+
+                break
+            case 'OldPasswordIsNotMatch':
+                Mobile.verifyElementVisible(findTestObject('Change Password/warn_old_password_is_not_match'), 0)
+
+                break
+            case 'InvalidNewPassword':
+                Mobile.verifyElementVisible(findTestObject('Change Password/warn_new_password_invalid'), 0)
+
+                break
+            case 'ConfirmNewPasswordIsNotMatch':
+                Mobile.verifyElementVisible(findTestObject('Change Password/warn_confirm_new_password_is_not_match'), 0)
+
+                break
+            case 'ErrorLengthPassword':
+                Mobile.verifyElementVisible(findTestObject('Change Password/warn_error_length_password'), 0)
+
+                break
+        }
+    }
+}
+
+Mobile.closeApplication()
+
--- Scripts/Forgot Password/Script1579688138936.groovy
+++ Scripts/Forgot Password/Script1579688138936.groovy
@@ -17,80 +17,80 @@ import internal.GlobalVariable as GlobalVariable
 
 Mobile.startApplication('F:\\Materi Kuliah (Universitas Atma Jaya Yogyakarta)\\MAGANG\\Task\\accpartner.apk', true)
 
-Mobile.tap(findTestObject('new/tap_forgot_password'), 0)
+Mobile.tap(findTestObject('Forgot Password/tap_forgot_password'), 0)
 
-Mobile.tap(findTestObject('new/tap_forgot_username'), 0)
+Mobile.tap(findTestObject('Forgot Password/tap_forgot_username'), 0)
 
-Mobile.setText(findTestObject('new/input_forgot_username'), var_forgot_username, 0)
+Mobile.setText(findTestObject('Forgot Password/input_forgot_username'), var_forgot_username, 0)
 
-Mobile.tap(findTestObject('new/button_forgot_confirm'), 0)
+Mobile.tap(findTestObject('Forgot Password/button_forgot_confirm'), 0)
 
 if (expected_username == 'passed') {
-    Mobile.verifyElementVisible(findTestObject('new/warn_forgot_verifikasi'), 0)
+    Mobile.verifyElementVisible(findTestObject('Forgot Password/warn_forgot_verifikasi'), 0)
 
     if (opt_otp == 'phone') {
-        Mobile.tap(findTestObject('new/tap_otp_phone_number'), 0)
+        Mobile.tap(findTestObject('Forgot Password/tap_otp_phone_number'), 0)
 
-        Mobile.setText(findTestObject('new/var_otp_1'), '2', 0)
+        Mobile.setText(findTestObject('Forgot Password/var_otp_1'), '2', 0)
 
-        Mobile.setText(findTestObject('new/var_otp_2'), '3', 0)
+        Mobile.setText(findTestObject('Forgot Password/var_otp_2'), '3', 0)
 
-        Mobile.setText(findTestObject('new/var_otp_3'), '2', 0)
+        Mobile.setText(findTestObject('Forgot Password/var_otp_3'), '2', 0)
 
-        Mobile.setText(findTestObject('new/var_otp_4'), '4', 0)
+        Mobile.setText(findTestObject('Forgot Password/var_otp_4'), '4', 0)
 
-        Mobile.setText(findTestObject('new/var_otp_5'), '2', 0)
+        Mobile.setText(findTestObject('Forgot Password/var_otp_5'), '2', 0)
 
-        Mobile.setText(findTestObject('new/var_otp_6'), '5', 0)
+        Mobile.setText(findTestObject('Forgot Password/var_otp_6'), '5', 0)
 
-        Mobile.tap(findTestObject('new/btn_verif_otp'), 0)
+        Mobile.tap(findTestObject('Forgot Password/btn_verif_otp'), 0)
     } else if (opt_otp == 'email') {
-        Mobile.tap(findTestObject('new/tap_otp_email'), 0)
+        Mobile.tap(findTestObject('Forgot Password/tap_otp_email'), 0)
 
-        Mobile.setText(findTestObject('new/var_otp_email_1'), '2', 0)
+        Mobile.setText(findTestObject('Forgot Password/var_otp_email_1'), '2', 0)
 
-        Mobile.setText(findTestObject('new/var_otp_email_2'), '3', 0)
+        Mobile.setText(findTestObject('Forgot Password/var_otp_email_2'), '3', 0)
 
-        Mobile.setText(findTestObject('new/var_otp_email_3'), '2', 0)
+        Mobile.setText(findTestObject('Forgot Password/var_otp_email_3'), '2', 0)
 
-        Mobile.setText(findTestObject('new/var_otp_email_4'), '4', 0)
+        Mobile.setText(findTestObject('Forgot Password/var_otp_email_4'), '4', 0)
 
-        Mobile.setText(findTestObject('new/var_otp_email_5'), '2', 0)
+        Mobile.setText(findTestObject('Forgot Password/var_otp_email_5'), '2', 0)
 
-        Mobile.setText(findTestObject('new/var_otp_email_6'), '5', 0)
+        Mobile.setText(findTestObject('Forgot Password/var_otp_email_6'), '5', 0)
 
-        Mobile.tap(findTestObject('new/btn_verif_otp_email'), 0)
+        Mobile.tap(findTestObject('Forgot Password/btn_verif_otp_email'), 0)
 
-        not_run: Mobile.tap(findTestObject('new/warn_empty_otp'), 0)
+        not_run: Mobile.tap(findTestObject('Forgot Password/warn_empty_otp'), 0)
 
-        not_run: Mobile.tap(findTestObject('new/warn_wrong_otp_email'), 0)
+        not_run: Mobile.tap(findTestObject('Forgot Password/warn_wrong_otp_email'), 0)
     }
     
-    Mobile.tap(findTestObject('new/android.widget.Button0 - Verifikasi'), 0)
+    Mobile.tap(findTestObject('Forgot Password/android.widget.Button0 - Verifikasi'), 0)
 
-    Mobile.tap(findTestObject('new/tap_new_password'), 0)
+    Mobile.tap(findTestObject('Forgot Password/tap_new_password'), 0)
 
-    Mobile.setText(findTestObject('new/input_new_password'), 'asda', 0)
+    Mobile.setText(findTestObject('Forgot Password/input_new_password'), 'asda', 0)
 
-    Mobile.tap(findTestObject('new/warn_is_empty_new_password'), 0)
+    Mobile.tap(findTestObject('Forgot Password/warn_is_empty_new_password'), 0)
 
-    Mobile.tap(findTestObject('new/input_confirm_password'), 0)
+    Mobile.tap(findTestObject('Forgot Password/input_confirm_password'), 0)
 
-    Mobile.setText(findTestObject('new/input_confirm_password'), 'asdfa', 0)
+    Mobile.setText(findTestObject('Forgot Password/input_confirm_password'), 'asdfa', 0)
 
-    Mobile.tap(findTestObject('new/warn_length_pass'), 0)
+    Mobile.tap(findTestObject('Forgot Password/warn_length_pass'), 0)
 
-    Mobile.tap(findTestObject('new/warn_new_password_is_not_match'), 0)
+    Mobile.tap(findTestObject('Forgot Password/warn_new_password_is_not_match'), 0)
 
-    Mobile.tap(findTestObject('new/warn_password_format'), 0)
+    Mobile.tap(findTestObject('Forgot Password/warn_password_format'), 0)
 
-    Mobile.tap(findTestObject('new/android.widget.Button0 - Konfirmasi'), 0)
+    Mobile.tap(findTestObject('Forgot Password/android.widget.Button0 - Konfirmasi'), 0)
 
     Mobile.closeApplication()
 } else if (expected_username == 'failed') {
-    Mobile.verifyElementVisible(findTestObject('new/warn_forgot_username_not_match'), 0, FailureHandling.OPTIONAL)
+    Mobile.verifyElementVisible(findTestObject('Forgot Password/warn_forgot_username_not_match'), 0, FailureHandling.OPTIONAL)
 
-    Mobile.verifyElementVisible(findTestObject('new/warn_forgot_username_is_empty'), 0, FailureHandling.OPTIONAL)
+    Mobile.verifyElementVisible(findTestObject('Forgot Password/warn_forgot_username_is_empty'), 0, FailureHandling.OPTIONAL)
 }
 
 Mobile.closeApplication()

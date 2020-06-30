5b95f55b710043899e7ee0124e7a7ea310ad33bc
Rizka Yulianti Pratiwi <rizkariz20@gmail.com>
2020-06-30 19:44:15 +0700

    Update Rizka - 30 Juni 2020

---
+++ Object Repository/Rizka/Daftar - KACAB dan ADMIN HEAD/tapObject.rs
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>tapObject</name>
+   <tag></tag>
+   <elementGuidId>08ca8ee5-5c74-46a2-a662-3e7856837674</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@class = 'android.view.View' and (contains(text(), '${text}') or contains(., '${text}'))]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>android.view.View</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>contains</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>${text}</value>
+   </webElementProperties>
+</WebElementEntity>
